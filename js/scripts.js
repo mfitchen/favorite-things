3260adc4761bd4359724604b75ae32d5c0ece0eb
Marty Fitchen and Matt Wilkin <matt@>
2016-06-06 10:55:56 -0700

    simplified js array code to accomplish the same thing.

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,15 @@ $(document).ready(function() {
     var q3 = $("input#fav3").val();
     var answers = [q1, q2, q3];
     var answersNew = [answers[1], answers[0], answers[2]];
+    // var answersFinal = [answers.push(answersNew[0], answersNew[1], answersNew[2])];
+    answers.push(answersNew[0], answersNew[1], answersNew[2]);
 
-    $(".fav2").text(answersNew[0]);
-    $(".fav1").text(answersNew[1]);
-    $(".fav3").text(answersNew[2]);
+    $(".fav1").text(answers[1]);
+    $(".fav2").text(answers[0]);
+    $(".fav3").text(answers[2]);
+    $(".fav4").text(answers[4]);
+    $(".fav5").text(answers[3]);
+    $(".fav6").text(answers[5]);
 
     $("#result-array").show();
 
